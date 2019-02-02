d030290ee30cd9fccc41d02725c15369e633d155
Paul Chen <lancevdance@gmail.com>
2019-02-02 18:08:31 +0800

    Buggy ChromeProcessHelper..

--- src/DumbPrograms.ChromeDevTools.Sample/Program.cs
+++ src/DumbPrograms.ChromeDevTools.Sample/Program.cs
@@ -9,30 +9,30 @@ namespace DumbPrograms.ChromeDevTools.Sample
     {
         static async Task Main(string[] args)
         {
-            var psi = new ProcessStartInfo(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "--remote-debugging-port=9222");
-            var process = Process.Start(psi);
-
-            var client = new ChromeDevToolsClient(9222);
-
-            var targets = from t in await client.GetInspectableTargets()
-                          where t.Title == "New Tab"
-                          select t;
-
-            var inspector = await client.Inspect(targets.First());
-
-            await inspector.Page.Enable();
-
-            var tcs = new TaskCompletionSource<bool>();
-
-            inspector.Page.LoadEventFired += async e =>
+            using (var chrome = new ChromeProcessHelper())
             {
-                Console.WriteLine($"Page loaded at {e.Timestamp}");
-                tcs.SetResult(true);
-            };
+                var client = await chrome.StartDevToolsClient();
 
-            var p = await inspector.Page.Navigate("https://www.baidu.com");
+                var targets = from t in await client.GetInspectableTargets()
+                                where t.Title == "New Tab"
+                                select t;
 
-            await tcs.Task;
+                var inspector = await client.Inspect(targets.First());
+
+                await inspector.Page.Enable();
+
+                var tcs = new TaskCompletionSource<bool>();
+
+                inspector.Page.LoadEventFired += async e =>
+                {
+                    Console.WriteLine($"Page loaded at {e.Timestamp.Value}");
+                    tcs.SetResult(true);
+                };
+
+                var p = await inspector.Page.Navigate("https://www.baidu.com");
+
+                await tcs.Task;
+            }
         }
     }
 }
---
+++ src/DumbPrograms.ChromeDevTools/ChromeProcessHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DumbPrograms.ChromeDevTools
+{
+    public class ChromeProcessHelper : IDisposable
+    {
+        private Process Chrome;
+
+        public int DebuggingPort { get; }
+        public bool Headless { get; }
+        public bool DisableGpu { get; }
+
+        public ChromeProcessHelper(int debuggingPort = 9222, bool headless = false, bool disableGpu = false)
+        {
+            DebuggingPort = debuggingPort;
+            Headless = headless;
+            DisableGpu = disableGpu;
+        }
+
+        public async Task<ChromeDevToolsClient> StartDevToolsClient(string url = null)
+        {
+            var psi = new ProcessStartInfo(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "--remote-debugging-port=9222");
+            Chrome = Process.Start(psi);
+
+            var client = new ChromeDevToolsClient(DebuggingPort);
+
+            if (!String.IsNullOrWhiteSpace(url))
+            {
+                await client.NewTab(url);
+            }
+
+            return client;
+        }
+
+        #region IDisposable Support
+
+        private bool Disposed = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!Disposed)
+            {
+                if (disposing)
+                {
+                    // dispose managed state (managed objects).
+
+                    Chrome.CloseMainWindow();
+                    Chrome.Dispose();
+                }
+
+                // set large fields to null.
+                Chrome = null;
+
+                Disposed = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+
+        #endregion
+    }
+}
